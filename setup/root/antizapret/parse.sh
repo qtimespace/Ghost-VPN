39c7d5727b84e56af2b0b2ae78f0d416038c8cd6
GubernievS <tyman@ya.ru>
2026-03-19 16:05:26 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-###
-
-if [[ -f /etc/openvpn/server/scripts/client-connect.sh ]]; then
-  echo '#!/bin/bash
-if [[ ! -v duplicate_cn ]]; then
-	for srv in antizapret-udp antizapret-tcp vpn-udp vpn-tcp; do
-		[[ "$dev" == "$srv" ]] && continue
-		echo "kill $common_name" \
-		| socat - "UNIX-CONNECT:/run/openvpn-server/${srv}.sock" \
-		| tail -n +2 \
-		| sed "s/^/$srv kill /" &
-	done
-fi
-if [[ "$IV_PLAT" != "linux" ]]; then
-	echo '\''push "ifconfig-ipv6 fdea:dead::2/64 fdea:dead::1"
-push "route-ipv6 ::/1"
-push "route-ipv6 8000::/1"
-push "block-ipv6"'\'' >> "$1"
-fi
-exit 0' > /etc/openvpn/server/scripts/client-connect.sh
-  chmod +x /etc/openvpn/server/scripts/client-connect.sh
-fi
-
-###
-
 set -e
 shopt -s nullglob
 
